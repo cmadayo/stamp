1fe4bb5b110c74e6e5b749464d859678730b784e
cmadayo <shuk0623app@gmail.com>
2021-03-07 10:45:11 +0900

    [Refactoring]Modify layout

--- app/src/main/java/com/example/stamp/ui/fragments/StampFragment.kt
+++ app/src/main/java/com/example/stamp/ui/fragments/StampFragment.kt
@@ -9,7 +9,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.stamp.R
 import com.example.stamp.databinding.FragmentStampBinding
@@ -56,7 +56,7 @@ class StampFragment : Fragment() {
             // TODO
         })
         binding.stampsView.apply {
-            layoutManager = LinearLayoutManager(context)
+            layoutManager = GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
             adapter = viewModelAdapter
         }
 
